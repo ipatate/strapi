4619f079cad1c4b0c8175abe49753cf74575c663
mfrachet <marvin.frachet@strapi.io>
2021-03-04 17:01:38 +0100

    middleware

--- packages/strapi-plugin-i18n/admin/src/middlewares/index.js
+++ packages/strapi-plugin-i18n/admin/src/middlewares/index.js
@@ -1,6 +1,12 @@
 import extendCTBInitialDataMiddleware from './extendCTBInitialDataMiddleware';
 import extendCTBAttributeInitialDataMiddleware from './extendCTBAttributeInitialDataMiddleware';
+import i18nMiddleware from './localeQueryParamsMiddleware'
 
-const middlewares = [extendCTBInitialDataMiddleware, extendCTBAttributeInitialDataMiddleware];
+
+const middlewares = [
+  extendCTBInitialDataMiddleware,
+  extendCTBAttributeInitialDataMiddleware,
+  i18nMiddleware,
+];
 
 export default middlewares;
---
+++ packages/strapi-plugin-i18n/admin/src/middlewares/localeQueryParamsMiddleware.js
@@ -0,0 +1,21 @@
+import get from 'lodash/get';
+
+const i18nMiddleware = () => ({ getState }) => next => action => {
+  if (action.type === 'ContentManager/ListView/SET_LIST_LAYOUT ') {
+    if (get(action, 'contentType.pluginOptions.i18n.localized', false)) {
+      if (action.initialParams.pluginOptions) {
+        action.initialParams.pluginOptions.localized = 'en';
+      } else {
+        action.initialParams.pluginOptions = {
+          localized: 'en',
+        };
+      }
+
+      return next(action);
+    }
+  }
+
+  return next(action);
+};
+
+export default i18nMiddleware;
